eb3fcce27fe6925cae2b547254fd2126cee40116
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-10 11:21:30 -0600

    descriptive message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I think I'm doing this right!");
+Console.WriteLine("Hello, World! I think I'm doing this right!"+Environment.NewLine+"Lets see if this works.");
